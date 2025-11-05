b57c17cae233e8af4311aedf5435cccbb60f81a6
eastlight82 <bigstake82@gmail.com>
2025-11-05 19:12:11 +0900

    init commit

---
+++ src/main/java/com/workingdead/WorkingdeadApplication.java
@@ -0,0 +1,13 @@
+package com.workingdead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingdeadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkingdeadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workingdead/WorkingdeadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workingdead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkingdeadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
